ac35abc0d4a741751c48fb09456b7f29f180fe28
Robert Li <robertlitech@gmail.com>
2019-11-13 16:48:27 -0500

    Moved the cv part into a single function, working on simulating things correctly

--- ImgToObj.py
+++ ImgToObj.py
@@ -2,24 +2,31 @@ import numpy as np
 import phyre
 import cv2
 import matplotlib.pyplot as plt
+import triangle as tri
+from enum import Enum
+
+class Layer(Enum):
+  background = 0
+  action = 1
+  object = 2
+  dynamic_goal = 3
+  static_goal = 4
+  dynamic_body = 5
+  static_body = 6
 
 def phyreImgToBoolLayers(img):
+  assert len(img.shape) == 2
   ncols = phyre.creator.constants.NUM_COLORS
   labels_one_hot = (img.ravel()[np.newaxis] == np.arange(ncols)[:, np.newaxis]).T
   labels_one_hot.shape = img.shape + (ncols,)
   return labels_one_hot
 
-def findContoursInPhyre(img):
-  allContours = []
-  layeredImg = phyreImgToBoolLayers(img)
-  for i in range(phyre.creator.constants.NUM_COLORS):
-    contours = []
-    if i > 0:
-      grayImg = np.full_like(layeredImg[:,:,i],255,dtype=np.uint8)
-      grayImg = np.where(layeredImg[:,:,i],grayImg,0)
-      contours,hierarchy = cv2.findContours(grayImg,cv2.RETR_LIST,cv2.CHAIN_APPROX_SIMPLE)
-    allContours.append(contours)
-  return allContours
+def phyreActionToPixelAction(action):
+  pixelAction = [0,0,0]
+  pixelAction[0] = action[0] * 255.0
+  pixelAction[1] = action[1] * 255.0
+  pixelAction[2] = action[2] * 30.0 + 1.9
+  return pixelAction
 
 def isContourCircle(contour):
   M = cv2.moments(contour)
@@ -28,25 +35,75 @@ def isContourCircle(contour):
 
 def seperateCircleFromOtherContours(contours):
   circles = []
-  otherContours = []
+  nonCircles = []
   for contour in contours:
+    contour = np.squeeze(contour)
     if isContourCircle(contour):
       (x,y),radius = cv2.minEnclosingCircle(contour)
       circles.append((x,y,radius))
     else:
-      otherContours.append(contour)
-  return (otherContours,circles)
+      nonCircles.append(contour)
+  return (nonCircles,circles)
 
 def simplifyContours(contours):
   simpleContours = []
   for contour in contours:
-    minLength = np.linalg.norm(contour[0,:,:] - contour[-1,:,:])
-    for i in range(contour.shape[0]-1):
-      length = np.linalg.norm(contour[i,:,:] - contour[i+1,:,:])
-
-    epsilon = 0.025*cv2.arcLength(contour,True)
-    simpleContours.append(cv2.approxPolyDP(contour,epsilon,True))
-    print(contour.shape,cv2.approxPolyDP(contour,epsilon,True).shape)
+    if contour.shape[0] > 4:
+      simpleContours.append(np.squeeze(cv2.approxPolyDP(contour,0.5,True)))
+    else:
+      simpleContours.append(contour)
   return simpleContours
 
-  
+def triangulateContour(contour):
+  verts = []
+  segs = []
+  for i in range(contour.shape[0]):
+    verts.append((contour[i,0],contour[i,1]))
+    segs.append((i,(i+1)%contour.shape[0]))
+  A = dict(vertices=verts, segments=segs)
+  B = tri.triangulate(A,'p')
+  return B
+
+# Turns the phyre scene image into geometric objects
+# Returns a list of objects per layer as dict:
+# 'raw_img': a grayscaled img of the layer
+# 'circles': a list of all the circles in the layer as a tuple (x,y,radius)
+# 'polygons': a list of all the polygons in the layer as a tuple of (n by 2 array of points,triangle dict)
+def phyreToObj(img,action):
+  # Turn the labels in a one hot vector img
+  layered_img = phyreImgToBoolLayers(img)
+
+  # Extract the contours for each layer, not including the background layer
+  scene_objects = []
+
+  for i in range(layered_img.shape[2]):
+    # Turn the boolean img into a greyscale img
+    gray_layer = np.full_like(layered_img[:,:,i],255,dtype=np.uint8)
+    gray_layer = np.where(layered_img[:,:,i],gray_layer,0)
+    circles = []
+    polygons = []
+
+    if i != Layer.background.value:
+      # Extract the contours
+      contours,hierarchy = cv2.findContours(gray_layer,cv2.RETR_LIST,cv2.CHAIN_APPROX_SIMPLE)
+
+      # Seperate circles from the contours
+      contours,circles = seperateCircleFromOtherContours(contours)
+      if i == Layer.action.value:
+        circles.append(phyreActionToPixelAction(action))
+
+      # Simplify the non circle contours
+      simple_contours = contours #simplifyContours(contours)
+
+      for contour in simple_contours:
+        polygon_tri = triangulateContour(contour)
+        polygons.append((contour,polygon_tri))
+
+    scene_objects.append(dict(raw_img=gray_layer,circles=circles,polygons=polygons))
+  return scene_objects
+
+
+
+
+
+
--- PixelsToObjTest.py
+++ PixelsToObjTest.py
@@ -1,10 +1,14 @@
 import math
 import random
+import time
 
 import cv2
 
 import numpy as np
 import matplotlib.pyplot as plt
+from matplotlib.patches import Polygon
+from matplotlib.collections import PatchCollection
+
 import pymunk
 from pymunk import Vec2d
 
@@ -13,18 +17,23 @@ import phyre
 import ImgToObj
 
 eval_setup = 'ball_cross_template'
-fold_id = 0  # For simplicity, we will just use one fold for evaluation.
+fold_id = 300  # For simplicity, we will just use one fold for evaluation.
 train_tasks, dev_tasks, test_tasks = phyre.get_fold(eval_setup, fold_id)
 action_tier = phyre.eval_setup_to_action_tier(eval_setup)
 print("Dev Tasks:{}".format(len(dev_tasks)))
 tasks = dev_tasks
+
+with open("tasks.txt","w") as f:
+  for i,task in enumerate(tasks):
+    print(i,task,file=f)
+
 simulator = phyre.initialize_simulator(tasks, action_tier)
-task_index = 100  # Note, this is a integer index of task within simulator.task_ids.
+task_index = 0  # Note, this is a integer index of task within simulator.task_ids.
 task_id = simulator.task_ids[task_index]
 initial_scene = simulator.initial_scenes[task_index]
 actions = simulator.build_discrete_action_space(max_actions=100)
 #action = random.choice(actions)
-action = [.5,.5,.1]
+action = [.7,.75,1.0]
 status, images = simulator.simulate_single(task_index, action, need_images=True)
 
 print('Result of taking action', action, 'on task', tasks[task_index], 'is:',
@@ -34,30 +43,30 @@ print('Is', action, 'an invalid action on task', tasks[task_index], '?',
       status.is_invalid())
 
 img_start = phyre.vis.observations_to_float_rgb(images[0])
-imgLayers = ImgToObj.phyreImgToBoolLayers(images[0])
+
 fig, axs = plt.subplots(3, 3, figsize=(7, 7))
 fig.tight_layout()
 plt.subplots_adjust(hspace=0.4, wspace=0.3)
 
-allCountours = ImgToObj.findContoursInPhyre(images[0])
-
-allOtherContours = []
-allCircles = []
-for contours in allCountours:
-  otherContours,circles = ImgToObj.seperateCircleFromOtherContours(contours)
-  allOtherContours.append(ImgToObj.simplifyContours(otherContours))
-  allCircles.append(circles)
+t0 = time.time()
+scene_objects = ImgToObj.phyreToObj(initial_scene,action)
+t1 = time.time()
+print(t1-t0,"Contour Finding Time")
 
 for i,ax in enumerate(axs.flatten()):
   ax.get_xaxis().set_ticks([])
   ax.get_yaxis().set_ticks([])
   if i < 7:
-    grayImg = np.full_like(imgLayers[:,:,i],255,dtype=np.uint8)
-    grayImg = np.where(imgLayers[:,:,i],grayImg,0)
-    grayImg = np.stack((grayImg,)*3, axis=-1)
-    grayImg = cv2.drawContours(grayImg,allOtherContours[i],-1,(0,255,0))
-    ax.imshow(grayImg)
-    for circle in allCircles[i]:
+    colorImg = np.stack((scene_objects[i]['raw_img'],)*3, axis=-1)
+    ax.imshow(colorImg)
+    for polygon in scene_objects[i]['polygons']:
+      ax.scatter(polygon[0][:,0],polygon[0][:,1],s=4,c='r')
+      for triangle in polygon[1]['triangles'].tolist():
+        ax.plot(polygon[1]['vertices'][triangle,0],polygon[1]['vertices'][triangle,1],'b')
+        ax.plot(polygon[1]['vertices'][[triangle[0],triangle[-1]],0],polygon[1]['vertices'][[triangle[0],triangle[-1]],1],'b')
+    ax.title.set_text(str(i))
+    for circle in scene_objects[i]['circles']:
+      print(i,circle)
       circle1=plt.Circle((circle[0],circle[1]),radius=circle[2],color='r',fill=False)
       ax.add_artist(circle1)
   if i == 7:
@@ -66,27 +75,14 @@ for i,ax in enumerate(axs.flatten()):
     ax.imshow(images[0])
 plt.show()
 
-# Call this first
-
+t0 = time.time()
 # Initialize space
 space = pymunk.Space()
-space.gravity = (0.0, -900.0)
+space.gravity = (0.0, -50.0)
+space.iterations = 10
 
-balls = []
-
-# Make the balls
-for circles in allCircles:
-  for circle in circles:
-    mass = 1
-    radius = circle[2]
-    inertia = pymunk.moment_for_circle(mass, 0, radius, (0,0))
-    body = pymunk.Body(mass, inertia)
-    body.position = circle[0],circle[1]
-    shape = pymunk.Circle(body, radius, Vec2d(0,0))
-    shape.elasticity = 0.5
-    shape.friction = 0.5
-    space.add(body, shape)
-    balls.append(shape)
+COLLISION_OBJECT = 1
+COLLISION_GOAL = 2
 
 # Make the walls
 static_body = space.static_body
@@ -95,20 +91,131 @@ static_lines = [pymunk.Segment(static_body, (0.0, 0.0), (0.0, 256.0), 0.0),
                 pymunk.Segment(static_body, (256.0, 256.0), (256.0, 0.0), 0.0),
                 pymunk.Segment(static_body, (256.0, 0.0), (0.0, 0.0), 0.0)]
 for line in static_lines:
-  line.elasticity = 0.5
-  line.friction = 0.9
+  line.elasticity = 0.3
+  line.friction = 0.6
 space.add(static_lines)
 
-for i in range(300):
-  space.step(1.0 / 60.0)
+# Add the scene objects to the space
+for i,layer_objects in enumerate(scene_objects):
+  # Add the balls
+  layer_objects['balls'] = []
+  for circle in layer_objects['circles']:
+    radius = circle[2]
+    body = pymunk.Body()
+    if i == ImgToObj.Layer.static_goal.value or i == ImgToObj.Layer.static_body.value:
+      body = pymunk.Body(body_type=pymunk.Body.STATIC)
+    body.position = circle[0],circle[1]
+    shape = pymunk.Circle(body, radius, Vec2d(0,0))
+    shape.elasticity = 0.3
+    shape.friction = 0.6
+    shape.density = 1
+    if i == ImgToObj.Layer.object.value:
+      shape.collision_type = COLLISION_OBJECT
+    elif i == ImgToObj.Layer.dynamic_goal.value or i == ImgToObj.Layer.static_goal.value:
+      shape.collision_type = COLLISION_GOAL
+    space.add(body, shape)
+    layer_objects['balls'].append((shape,[body.position]))
+
+  # Add the polygons to the space
+  # A list of lists of triangles per polygon
+  layer_objects['pymunk_polys'] = []
+  for polygon in layer_objects['polygons']:
+    triangles = []
+    center = np.mean(polygon[1]['vertices'].astype(float),axis=0)
+    body = pymunk.Body()
+    if i == ImgToObj.Layer.static_goal.value or i == ImgToObj.Layer.static_body.value:
+      body = pymunk.Body(body_type=pymunk.Body.STATIC)
+    body.position = center
+    for triangle in polygon[1]['triangles'].tolist():
+      shape = pymunk.Poly(body,polygon[1]['vertices'][triangle,:].astype(float)-center,radius=1.0)
+      shape.elasticity = 0.3
+      shape.friction = 0.6
+      shape.density = 1
+      if i == ImgToObj.Layer.object.value:
+        shape.collision_type = COLLISION_OBJECT
+      elif i == ImgToObj.Layer.dynamic_goal.value or i == ImgToObj.Layer.static_goal.value:
+        shape.collision_type = COLLISION_GOAL
+      space.add(shape)
+      triangles.append(shape)
+    space.add(body)
+    layer_objects['pymunk_polys'].append(triangles)
+
+dt = 1.0/60.0
+time_sim = len(images)
+num_steps = int(time_sim/dt)
+steps_per_sec = int(num_steps/time_sim)
+
+print("Seconds Simulated",time_sim)
+print("Steps Simulated",num_steps)
+print("Time Step",dt)
+
+for i in range(num_steps):
+  space.step(dt)
+  # Delay fixed time between frames
+  for layer_objects in scene_objects:
+    for ballData in layer_objects['balls']:
+      ballData[1].append(ballData[0].body.position)
+
+  if i % steps_per_sec == 0:
+    img_end = phyre.vis.observations_to_float_rgb(images[int(i/steps_per_sec)])
+    fig, axs = plt.subplots(1,2)
+    axs[0].imshow(img_end)
+    axs[1].imshow(np.full_like(img_end,255))
+    axs[0].title.set_text('{} sec out of {}'.format(int(i/steps_per_sec),time_sim))
+    patches = []
+    for layer_objects in scene_objects:
+      for ballData in layer_objects['balls']:
+        circle1=plt.Circle((ballData[0].body.position.x,256.0-ballData[0].body.position.y),radius=ballData[0].radius,color='b',fill=True)
+        axs[1].add_artist(circle1)
+
+      for pymunk_poly in layer_objects['pymunk_polys']:
+        for triangle in pymunk_poly:
+          verts = []
+          for v in triangle.get_vertices():
+            x,y = v.rotated(triangle.body.angle) + triangle.body.position
+            verts.append([x,y])
+          verts = np.array(verts)
+          verts[:,1] = 256.0-verts[:,1]
+          polygon = Polygon(verts,color='b')
+          patches.append(polygon)
+
+    p1 = PatchCollection(patches)
+    p2 = PatchCollection(patches, alpha=0.4)
+    axs[1].add_collection(p1)
+    axs[0].add_collection(p2)
+    plt.show()
+
+t1 = time.time()
+print(t1-t0,"Sim Time")
+exit()
+
 
 img_end = phyre.vis.observations_to_float_rgb(images[-1])
-fig, ax = plt.subplots()
-ax.imshow(img_end)
+fig, axs = plt.subplots(1,2)
+axs[0].imshow(img_end)
+axs[1].imshow(np.full_like(img_end,255))
 
-for ball in balls:
-  circle1=plt.Circle((ball.body.position.x,256.0-ball.body.position.y),radius=ball.radius,color='b',fill=False)
-  ax.add_artist(circle1)
+
+for i,ball in enumerate(balls):
+  circle1=plt.Circle((ball.body.position.x,256.0-ball.body.position.y),radius=ball.radius,color='b',fill=True)
+  axs[1].add_artist(circle1)
+  ballTrail = np.array(ballPos[i])
+  axs[1].plot(ballTrail[:,0],256.0-ballTrail[:,1])
+
+
+patches = []
+
+for triangle in triangles:
+  verts = []
+  for v in triangle.get_vertices():
+    x,y = v.rotated(triangle.body.angle) + triangle.body.position
+    verts.append([x,y])
+  verts = np.array(verts)
+  verts[:,1] = 256.0-verts[:,1]
+  polygon = Polygon(verts,color='b')
+  patches.append(polygon)
+p = PatchCollection(patches, alpha=0.4)
+axs[1].add_collection(p)
 
 plt.show()
-  
+
